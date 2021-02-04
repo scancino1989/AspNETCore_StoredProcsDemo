966bb2b119bdc9e09870c4c5bf81190312fedf0b
scancino1989 <78565956+scancino1989@users.noreply.github.com>
2021-02-04 15:43:15 -0500

    Add files via upload

---
+++ AspNETCore_StoredProcsDemo/Data/ValuesRepository.cs
@@ -0,0 +1,31 @@
+﻿using AspNETCore_StoredProcsDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNETCore_StoredProcsDemo.Data
+{
+    public class ValuesRepository
+    {
+        public Task<List<Value>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Value>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(Value value)
+        {   
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AspNETCore_StoredProcsDemo/Models/Value.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNETCore_StoredProcsDemo.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public int Value1 { get; set; }
+
+        public string Value2 { get; set; }
+    }
+}
